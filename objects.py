401c35fde5f943ecce579e72a7483a798d25d4ba
Higor Fellipe de Almeida Silva <higor_silva@usp.br>
2022-06-18 00:49:01 -0300

    Adicionado as matrizes iniciais

--- objects.py
+++ objects.py
@@ -1,12 +1,13 @@
 class bar:
-    def __init__(self, E=0, A=0, L=0, pho=0, node1=0, node2=0,theta=0):
-        self.E = E #bar's elastic module
-        self.A = A #bar's section area
-        self.L = L #bar's size
-        self.pho = pho #bar's density
-        self.node1 = node1 #first bar's node
+    def __init__(self, E=0, A=0, L=0, pho=0, I = 0, node1=0, node2=0,theta=0):
+        self.E = E          #bar's elastic module
+        self.A = A          #bar's section area
+        self.L = L          #bar's size
+        self.pho = pho      #bar's density
+        self.I = I          #Inertia
+        self.node1 = node1  #first bar's node
         self.node2 = node2
-        self.theta = theta #bar's angle to the horizontal
+        self.theta = theta  #bar's angle to the horizontal
 
     @classmethod
 
@@ -15,6 +16,7 @@ class bar:
         A = float(input("Indique a área da seção da barra: "))
         L = float(input("Indique o comprimeto da barra: "))
         pho = float(input("Indique a densidade da barra: "))
+        I = float(input("Indique a constante de inércia da barra: "))
         node1 = int(input("Indique o primeiro nó relacionado a barra: "))
         node2 = int(input("Indique o segundo nó relacionado a barra: "))
         theta = float(input("Indique o ânglo da barra: "))
---
+++ porticos/matrices.py
@@ -0,0 +1,48 @@
+from objects import bar
+import numpy as np
+
+def get_Ke_t(bar): #Matriz de rigidez da treliça
+    Ke_t = (bar.E*bar.A/bar.L)*np.array[[ 1, 0, 0, -1, 0, 0], 
+                                        [ 0, 0, 0,  0, 0, 0],
+                                        [ 0, 0, 0,  0, 0, 0],
+                                        [-1, 0, 0,  1, 0, 0],
+                                        [ 0, 0, 0,  0, 0, 0],
+                                        [ 0, 0, 0,  0, 0, 0]] 
+    return Ke_t
+
+def get_Ke_v(bar): #Matriz de rigidez da viga
+    Ke_v = (bar.E*bar.I/(bar.L**3))*np.array[[0,       0,          0, 0,        0,          0]  
+                                              [0,      12,    6*bar.L, 0,      -12,    6*bar.L]
+                                              [0, 6*bar.L, 4*bar.L**2, 0, -6*bar.L, 2*bar.L**2]
+                                              [0,       0,          0, 0,        0,          0]
+                                              [0,     -12,   -6*bar.L, 0,       12,   -6*bar.L]
+                                              [0, 6*bar.L, 2*bar.L**2, 0, -6*bar.L, 4*bar.L**2]]
+    return Ke_v
+
+def get_Ke_p(bar): #Matriz de rigidez do pórtico
+    return get_Ke_t(bar) + get_Ke_v(bar) 
+
+
+
+def get_Me_t(bar): #Matriz de massa da treliça
+    Me_t = (bar.pho*bar.A*bar.L/6)*np.array[[2, 0, 0, 1, 0, 0], 
+                                            [0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0],
+                                            [1, 0, 0, 2, 0, 0],
+                                            [0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0]]
+    return Me_t
+
+def get_Me_v(bar): #Matriz de massa da viga
+    Me_v = (bar.pho*bar.A*bar.L/420)*np.array[[0,         0,           0, 0,         0,           0]  
+                                              [0,       156,    22*bar.L, 0,        54,   -13*bar.L]
+                                              [0,  22*bar.L,  4*bar.L**2, 0,  13*bar.L, -3*bar.L**2]
+                                              [0,         0,           0, 0,         0,           0]
+                                              [0,        54,    13*bar.L, 0,       156,   -22*bar.L]
+                                              [0, -13*bar.L, -3*bar.L**2, 0, -22*bar.L,  4*bar.L**2]]
+    return Me_v
+
+def get_Me_p(bar): #Matriz de massa do pórtico
+    return get_Me_t(bar) + get_Me_v(bar)
+
+T =   [] #Matriz de rotação  
