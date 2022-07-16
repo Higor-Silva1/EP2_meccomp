d64d7a508a355a333e65e068357fcc4ccd2c8528
Higor Fellipe de Almeida Silva <higor_silva@usp.br>
2022-07-16 02:26:39 -0300

    a

--- runEP.py
+++ runEP.py
@@ -1,12 +1,26 @@
+from Bar import Bar
+from Global import Global
+from porticos import matrices
+import numpy as np
+from porticos.dinamica import analise
+
 ###### Propriedades do sistema para montar as matrizes ######
 
 #Barras vermelhas
 
+Ev = 4; Av = 1; phov = 1; Iv = 1
+
 #Barras Azuis
+
+Ea = 4; Aa = 1; phoa = 1; Ia = 1
 
 #Barras Cinzas
 
+Ec = 4; Ac = 1; phoc = 1; Ic = 1
+
 #Barras Roxas
+
+Er = 4; Ar = 1; phor = 1; Ir = 1
 
 ###### Resolver o sistema ######
 
